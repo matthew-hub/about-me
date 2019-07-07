c0d096877168d4084277d69364fd3ccdeaa9f776
Matthew <mateuszr430@gmail.com>
2019-07-07 04:16:12 +0200

    added me component

---
+++ src/components/Me.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Me.scss';
+const Me = () => {
+  return (
+    <div className="me">
+      <h1>Hello, I'm Matthew.</h1>
+      <p>I am a self-taught programmer. 
+        I coding in <span className="me js">JavaScript</span> and <span className="me rc">ReactJS</span>, learning new things, 
+        such as React Native, Nodejs etc.  </p>
+    </div>
+  );
+}
+
+export default Me;
