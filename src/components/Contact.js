8ff9933eaea7ed9dae8d3bcfbc37c07142cb4840
Matthew <mateuszr430@gmail.com>
2019-07-07 04:15:59 +0200

    added contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Contact.scss";
+const Contact = props => {
+  let styless = {
+    color: "#F82929",
+    borderBottom: "3px solid #F82929"
+  };
+
+  if (props.theme !== "light") {
+    styless.color = "#fff";
+  }
+
+  return (
+    <div className="contact">
+      <h1>Say Hello!</h1>
+      <p>
+        Thanks for stopping by!
+        <br />
+        I'd be happy to help you out!
+      </p>
+      <a href="mailto:hello@matthew.com">
+        <span style={styless}>hello@matthew.com</span>
+      </a>
+    </div>
+  );
+};
+
+export default Contact;
